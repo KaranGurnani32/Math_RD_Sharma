c1e2b2f84da06bb8b340aa0c85f861be6d67edaa
karan <karang393@gmail.com>
2021-12-30 10:50:20 +0530

    1. Write each of the following in numeral form: (ii) Seventy thousand fifty three.

--- Class6thMaths/src/Chapter1Questions.java
+++ Class6thMaths/src/Chapter1Questions.java
@@ -1,2 +1,10 @@
 public class Chapter1Questions {
+    //Exercise1.1
+    //1. Write each of the following in numeral form: (ii) Seventy thousand fifty three.
+    public static void main(String[] args) {
+
+        String SeventyThousandFiftyThree = "70,053";
+
+        System.out.println("The Numeric Form is : " + SeventyThousandFiftyThree);
+    }
 }
